65317c50880b4ceb02bb12e866bae0750df373f5
Sam Boyer <tech@samboyer.org>
2014-06-16 16:56:15 -0400

    Add a server-create route to the master server.

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/sdboyer/mock_druplatform_api/acquia"
@@ -11,21 +12,36 @@ import (
 var fml = fmt.Println
 
 func main() {
-	// Spawn an Acquia API server on the default port
-	aq := acquia.NewServerInstance()
-
-	an := negroni.New()
-	an.UseHandler(acquia.NewRouter(aq))
-
-	go an.Run(":10234")
-
 	router := mux.NewRouter()
+	router.Headers("Content-Type", "application/json")
 
 	router.HandleFunc("/", ServerListHandler)
+	router.HandleFunc("/server/create", hhCreateServer).Methods("POST")
 
 	n := negroni.New()
 	n.UseHandler(router)
 	n.Run(":10233")
+}
+
+type createServerRequest struct {
+	port int `json:"port"`
+	server_type string  `json:"server_type"`
+	version string `json:"version"`
+}
+
+func hhCreateServer(w http.ResponseWriter, r *http.Request) {
+	j := &createServerRequest{}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(j); err != nil {
+		panic(err)
+	}
+
+	an := negroni.New()
+	an.UseHandler(acquia.NewRouter(acquia.NewServerInstance("default")))
+
+	// TODO need to figure out how to get the port ahead of time
+	go an.Run(":10234")
 }
 
 func ServerListHandler(w http.ResponseWriter, r *http.Request) {
