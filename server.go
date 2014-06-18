3a8c7fd1d0209ffa21eb8a04b709377a1b251e09
Sam Boyer <tech@samboyer.org>
2014-06-17 21:34:15 -0400

    Incremental checkin - server creator is mostly there

--- server.go
+++ server.go
@@ -2,28 +2,51 @@ package main
 
 import (
 	"fmt"
+	//"log"
 	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/sdboyer/mock_druplatform_api/acquia"
 	"net/http"
+	"net"
+	"time"
 )
 
 var fml = fmt.Println
 
-type ServerIdentifier interface {
-	Port() int
-	Name() string
+// tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
+// connections. It's used by ListenAndServe and ListenAndServeTLS so
+// dead TCP connections (e.g. closing laptop mid-download) eventually
+// go away.
+//
+// Copied from net/http, b/c they did not deign to export
+type tcpKeepAliveListener struct {
+    *net.TCPListener
 }
 
-var servers = make(map[int]ServerIdentifier, 0)
+func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
+    tc, err := ln.AcceptTCP()
+    if err != nil {
+        return
+    }
+    tc.SetKeepAlive(true)
+    tc.SetKeepAlivePeriod(3 * time.Minute)
+    return tc, nil
+}
+
+type ServerInstance struct {
+	Listener tcpKeepAliveListener
+	HttpServer *http.Server
+}
+
+var servers = make([]ServerInstance, 0)
 
 func main() {
 	router := mux.NewRouter()
 	router.Headers("Content-Type", "application/json")
 
-	router.HandleFunc("/", ServerListHandler)
-	router.HandleFunc("/server/create", hhCreateServer).Methods("POST")
+	router.HandleFunc("/", hhListServers).Methods("GET")
+	router.HandleFunc("/", hhCreateServer).Methods("POST")
 
 	n := negroni.New()
 	n.UseHandler(router)
@@ -47,10 +70,26 @@ func hhCreateServer(w http.ResponseWriter, r *http.Request) {
 	an := negroni.New()
 	an.UseHandler(acquia.NewRouter(acquia.NewServerInstance("default")))
 
-	// TODO need to figure out how to get the port ahead of time
-	go an.Run(":10234")
+	laddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:0") // listen to all the things
+	if err != nil {
+		panic(err)
+	}
+
+	l, err := net.ListenTCP("tcp", laddr)
+	if err != nil {
+		panic(err)
+	}
+	kal := tcpKeepAliveListener{l}
+
+	// The Addr prop shouldn't actually be used, but set it to avoid triggering defaults
+	srv := &http.Server{Addr: laddr.String(), Handler: an}
+
+	go srv.Serve(kal)
+
+	si := ServerInstance{Listener: kal, HttpServer: srv}
+	servers = append(servers, si)
 }
 
-func ServerListHandler(w http.ResponseWriter, r *http.Request) {
+func hhListServers(w http.ResponseWriter, r *http.Request) {
 
 }
